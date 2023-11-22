71434085c01b417d16f626071a9466a560a033f6
Sahilchaudhari-dev <128718728+Sahilchaudhari-dev@users.noreply.github.com>
2023-11-22 15:59:49 +0000

    Nov 22, 2023, 9:29 PM

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import App2 from "./App2";
+function App() {
+  const name = "sahil";
+  return (
+    <>
+      <div className="App">shkdjhasgdjh</div>
+      <App2 name={name} />
+      <div>dbhfg</div>
+    </>
+  );
+}
+export default App;
---
+++ my-app/src/App2.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App2({name}) {
+  return (
+    <>
+      <h1>{name}</h1>
+    </>
+  );
+}
+
+export default App2;
